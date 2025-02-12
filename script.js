236eb102a439ff652f2d62f64580600dad7373aa
Silvia <silvia.serra.7e6@itb.cat>
2025-02-12 20:08:53 +0100

    finish game

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const GREY = "grey";
 const WHITE = "white";
 const BLACK = "black";
 
+const msj_sinIntentos = "No tienes más intentos :( "
+const msj_acertado = "Has acertado! Enhorabuena! :D "
+const msj_intento = "Intentos restantes: "
 
 //Declaración de variables globales.
 let master = [];
@@ -18,7 +21,7 @@ function init() {
     let result = document.querySelector('#result');
 
     //1. Llama al código random del master
-    master = generaAleatorio();
+    generaAleatorio();
     console.log(master);
 
     //2. Crea todas las filas según el número de intentos.
@@ -26,21 +29,16 @@ function init() {
 
 }
 
-function generaAleatorio(){
-
-        //1. Genera el código random del master
-        let combiGanadora = [];
-
-        for(let i = 0; i< MAX_COMBI_COLORES; i++){
-            let coloresAleatorios = COLORS[Math.floor(Math.random()* COLORS.length)];
-            combiGanadora.push(coloresAleatorios);
-        }
-        return combiGanadora;
+function generaAleatorio() {
+    //1. Genera el código random del master
+    for (let i = 0; i < MAX_COMBI_COLORES; i++) {
+        let coloresAleatorios = COLORS[Math.floor(Math.random() * COLORS.length)];
+        master.push(coloresAleatorios);
+    }
 }
 
 
 function crearFilas() {
-
     // Creamos filas según intentos
     for (let i = 0; i < MAX_INTENTOS; i++) {
         let filas = document.querySelector("#Result");
@@ -52,12 +50,77 @@ function crearFilas() {
 /* Llamaremos a esta función desde el botón HTML de la página para comprobar la propuesta de combinación que nos ha
 introducido el usuario.
 Informamos al usuario del resultado y del número de intentos que lleva*/
+
 function Comprobar() {
+    if (userCombi.length < 4 ) return;
+
+    if(intento + 1 >= MAX_INTENTOS) {
+        document.querySelector("#info").textContent = msj_sinIntentos;
+        revelarCombinacion();
+        return
+    }
+
+    if (comprobarCombinacion()){
+        document.querySelector("#info").textContent = msj_acertado;
+    }else{
+        intento++;
+        userCombi = [];
+        document.querySelector("#info").textContent = msj_intento + (MAX_INTENTOS - intento);
+    }    
+}
+
+// Revelar la combinación ganadora
+function revelarCombinacion() {
+    for (let color in master){
+     let mostrarMaster= document.querySelectorAll("#master .w100 .w25");
+      mostrarMaster[color].querySelector(".cel").style.background = master[color];
+    }
 }
 
 /** Procedimiento que se ejecuta cada vez que el usuario selecciona un color, hasta el número máximo de colores permitidos en la combinación. */
 function añadeColor(color) {
-   
+    //Máximo 4 colores
+    if (userCombi.length >= 4) return;
+
+    //Agregar color a la combinación del user
+    userCombi.push(color);
+
+    let rowResults = document.querySelectorAll("#Result div.rowResult");
+
+    let rowIntento = rowResults[intento];
+
+    let celda = rowIntento.querySelectorAll(".rowUserCombi div.w25");
+
+    celda[userCombi.length - 1].querySelector('.celUserCombi').style.background = color;
+}
+
+function comprobarCombinacion() {
+    let aciertos = 0;
+
+    for (let color in userCombi) {
+        let posColor = master.indexOf(userCombi[color])
+        if(posColor != -1){
+            //el color existe
+            if(color == posColor){
+               circuloColor(color).style.background = BLACK
+               aciertos++;
+            }else{
+                circuloColor(color).style.background = WHITE
+            }
+        }else{
+            //el color no existe
+            circuloColor(color).style.background = GREY
+        }
+    }
+    return aciertos == 4;
+}
+
+//devolver el elemento a pintar
+function circuloColor(color){
+    let rowResults = document.querySelectorAll("#Result div.rowResult");
+    let rowIntento = rowResults[intento];
+    let celda = rowIntento.querySelectorAll(".rowCercleResult div.w40");
+    return celda[color].querySelector('.cercleResult')
 }
 
 
